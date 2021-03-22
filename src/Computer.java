1cd355c0372ada4071a6a619dda3407f93ad76d7
saritha-kola <varshith.kola@gmail.com>
2021-03-22 11:38:03 +0100

    Created Laptop and StationarComputer

--- src/Computer.java
+++ src/Computer.java
@@ -1,4 +1,45 @@
 
-public class Computer {
-
+public class Computer implements ComputerOperations {
+	
+	private String className = " ";
+	
+	public Computer() {
+		this.className = "Computer";
+	}
+	
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getClassName() {
+		return this.className;
+	}
+	public void turnon() {
+		System.out.println(getClassName() + "Is now turned on");
+	}
+	
+	public void turnoff() {
+		System.out.println(getClassName() + "Is now turned off");
+		
+	}
+	public void performDownload() {
+		System.out.println(getClassName() + "Is now downloading a file");
+		
+		
+	}
+	public void performUpload() {
+		System.out.println(getClassName() + "Is now uploading a file");
+		
+		
+	}
+	public void playVideo() {
+		System.out.println(getClassName() + " Is now playing a video");
+		
+	}
+	public void playMusic() {
+		System.out.println(getClassName() + "Is now playing music");
+		
+		
+	}
+	
 }
+	
--- src/ComputerOperations.java
+++ src/ComputerOperations.java
@@ -1,4 +1,13 @@
 
 public interface ComputerOperations {
+	public void turnon();
+	public void turnoff();
+	public void performDownload();
+	public void performUpload();
+	public void playVideo();
+	public void playMusic();
+	
+		
+	}
 
-}
+
